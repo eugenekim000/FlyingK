e631d95fc225e1607ee1bf86eb98a9cf3870c9e0
Nao Arimura <naoyosetai@gmail.com>
2019-11-07 19:54:27 +0900

    wrote test but not passed

--- test/test.js
+++ test/test.js
@@ -1,1 +1,14 @@
-import axios from "axios";
+const axios = require("axios");
+
+describe("/api/locations", () => {
+  it("should return status 200", async () => {
+    const responce = await axios.get("/api/locations");
+    console.log(responce);
+    responce.should.have.status(200);
+  });
+
+  it("should return array of objects", async () => {
+    const responce = await axios.get("/api/locations");
+    responce.should.be.an("array");
+  });
+});
