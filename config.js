0e11974608294bf51ea018e64a9c888617af82b5
sidiousvic <victor6rene@gmail.com>
2019-11-08 08:13:33 +0900

    fixed migrations problem

--- config.js
+++ config.js
@@ -3,13 +3,7 @@ require("dotenv").config();
 module.exports = {
   db: {
     client: "pg",
-    connection: process.env.DATABASE_URL || {
-      host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || "trucko",
-      user: process.env.DB_USER || "VICSIDIOUS",
-      password: process.env.DB_PASSWORD || "VICSIDIOUS",
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
       directory: "./models/migrations",
