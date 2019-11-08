ddcd015a9e7e2a2fc3fe9a47de5273c30589bfa0
Daniel Paz <dannypazzy01@gmail.com>
2019-11-08 18:30:13 +0900

    Completed actions and reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,67 @@
 const defaultState = {
   locations: [],
+  backup: [],
 };
+
+const equal = (a, b) => a === b;
 
 const photos = (state = defaultState, action) => {
   switch (action.type) {
     case "SET_LOCATIONS":
-      return { ...state, locations: action.locations };
+      return {
+        ...state,
+        locations: action.locations,
+        backup: action.locations,
+      };
+    case "GET_STATES":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          equal(location.state, action.input)
+        ),
+      };
+    case "GET_CITIES":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          equal(location.city, action.input)
+        ),
+      };
+    case "GET_HIGHWAYS":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          equal(location.highway, action.input)
+        ),
+      };
+    case "GET_TRUCK_SERVICES":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          action.input.every((value) => location.truckServices.includes(value))
+        ),
+      };
+    case "GET_TYPE":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          action.input.every((value) => location.type.includes(value))
+        ),
+      };
+    case "GET_AMENITIES":
+      return {
+        ...state,
+        locations: state.locations.filter((location) =>
+          action.input.every((value) => location.amenities.includes(value))
+        ),
+      };
+    case "GET_":
+      return;
+    case "GO_HOME":
+      return {
+        ...state,
+        locations: state.backup,
+      };
     default:
       return state;
   }
